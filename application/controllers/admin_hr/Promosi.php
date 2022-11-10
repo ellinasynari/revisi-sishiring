f8750d4e2c1d465e101682fb0e1e72a86ccf3e7f
ellinasynari <59855035+ellinasynari@users.noreply.github.com>
2022-11-10 11:06:45 +0700

    Update Promosi.php

--- application/controllers/admin_hr/Promosi.php
+++ application/controllers/admin_hr/Promosi.php
@@ -3,42 +3,98 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Promosi extends CI_Controller {
 
-	function __construct()
-    {
-        parent::__construct();
-        $this->load->model('MUpload');
-        $this->load->model('MPromosi');
-        $this->load->model('MInput');
+	public function __construct()
+	{
+	    parent::__construct();
+        $this->load->helper(array('form', 'url'));
+		$this->load->model('MPromosi','MPromosi');
+		$this->load->model('MInput','MInput');
+		$this->load->model('MUpload','MUpload');
 
 
-    }
+        $this->load->library('form_validation');
+	}
 
-    /*
-    |-------------------------------------------------------------------
-    | Index
-    |-------------------------------------------------------------------
-    |
-    */
 	public function index()
 	{
-        $data['karyawan_list'] = $this->MUpload->fetch_karyawan();
+		$data = [
+            'promosi' => $this->MPromosi->tampil_semua(),
+			'jabatan_kosong' => $this->MInput->tampil_semua(),
+            //'karyawan_list' => $this->MUpload->fetch_karyawan(),
+			'karyawan' => $this->MUpload->tampil_semua(),
+
+
+        ];
+
 		$this->load->view('adminHr/template/v_header');
 		$this->load->view('adminHr/template/v_sidebar');
 		$this->load->view('adminHr/v_promosi', $data);
 		$this->load->view('adminHr/template/v_footer');
 	}
 
-    public function promosi_kandidat()
-    {
-	    $data = [
-		'promosi' => $this->MPromosi->tampil_semua(),
-		'jabatan_kosong' => $this->MInput->tampil_semua(),
-		'karyawan' => $this->MUpload->tampil_semua(),
-	];
+	public function promosi_kandidat()
+	{
+		$data = [
+            'promosi' => $this->MPromosi->tampil_semua(),
+			'jabatan_kosong' => $this->MInput->tampil_semua(),
+            'karyawan_list' => $this->MUpload->fetch_karyawan(),
+
+        ];
+
 		$this->load->view('adminHr/template/v_header');
 		$this->load->view('adminHr/template/v_sidebar');
-		$this->load->view('adminHr/v_promosi_kandidat', $data);
+		$this->load->view('adminHr/v_kandidat_lolos', $data);
 		$this->load->view('adminHr/template/v_footer');
 	}
-}
 
+	public function tambah()
+    {
+
+		$data['jabatan_kosong'] = $this->MInput->tampil_semua();
+		$data['karyawan'] = $this->MUpload->tampil_semua();
+        $data['promosi'] = $this->MPromosi->tampil_semua();
+        $this->load->view('adminHr/template/v_header');
+		$this->load->view('adminHr/template/v_sidebar');
+		$this->load->view('adminHr/modal-pengajuan', $data);
+		$this->load->view('adminHr/template/v_footer');      
+    }
+
+	public function save(){
+
+        $id = $this->input->post('id');
+        $id_jabatan_kosong = $this->input->post('id_jabatan_kosong');
+        $id_karyawan = $this->input->post('id_karyawan');
+		$jadwal = $this->input->post('jadwal');
+		$nilai_kreatif = $this->input->post('nilai_kreatif');
+		$nilai_kerjasama = $this->input->post('nilai_kerjasama');
+		$nilai_kemampuan_bekerja = $this->input->post('nilai_kemampuan_bekerja');
+		$nilai_kepemimpinan = $this->input->post('nilai_kepemimpinan');
+		$nilai_norma = $this->input->post('nilai_norma');
+		$grade = $this->input->post('grade');
+		
+
+        $data = array(
+            'id' => $id,
+            'id_jabatan_kosong' => $id_jabatan_kosong,
+            'id_karyawan' => $id_karyawan,
+			'jadwal' => $jadwal,
+			'nilai_kreatif' => $nilai_kreatif,
+			'nilai_kerjasama' => $nilai_kerjasama,
+			'nilai_kemampuan_bekerja' => $nilai_kemampuan_bekerja,
+			'nilai_kepemimpinan' => $nilai_kepemimpinan,
+			'nilai_norma' => $nilai_norma,
+			'grade' => $grade,
+
+
+        );
+
+        $this->MPromosi->save($data,'promosi');
+        redirect('admin_hr/promosi');
+    }
+
+     public function delete ($id) {
+        $this->MPromosi->delete($id);
+        redirect('admin_hr/promosi');
+     }
+
+}
